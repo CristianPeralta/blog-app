3a0199efade638a842f3bcebdcc1697c1515e398
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-24 09:30:25 -0500

    add default baseurl for axios request

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -17,7 +17,7 @@ class NewPost extends Component {
             author: this.state.author,
             userId: 1
         };
-        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        axios.post('/posts', data)
         .then(response => {
             console.log(response);
         });
