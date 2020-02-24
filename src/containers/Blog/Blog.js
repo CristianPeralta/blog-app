ef44a33800f2c6ec30ef65319ab9713bbb0a54b5
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-24 07:17:57 -0500

    render post author

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -29,7 +29,7 @@ class Blog extends Component {
     render () {
         const posts = this.state.posts.map(post => {
             return (
-                <Post key={post.id} title={post.title} />
+                <Post key={post.id} title={post.title} author={post.author}/>
             );
         })
         return (
