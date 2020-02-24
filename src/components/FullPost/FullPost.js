1a17827319932a8ecab42a4615559cf33af82ecd
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-24 08:01:27 -0500

    avoid infinite loop

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -9,7 +9,7 @@ class FullPost extends Component {
     }
 
     componentDidUpdate() {
-        if (this.props.id) {
+        if (!this.state.loadedPost || (this.state.loadedPost.id !== this.props.id)) {
             axios.get('https://jsonplaceholder.typicode.com/posts/'+ this.props.id)
             .then(response => {
                 this.setState({
@@ -26,8 +26,8 @@ class FullPost extends Component {
         if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
