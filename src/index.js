04946d942670a7771fc4408eea6e2d7914f7bb3d
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-24 09:25:39 -0500

    thorw error with interceptor

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import axios from 'axios';
 
 axios.interceptors.request.use(request => {
     console.log(request);
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
 });
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
