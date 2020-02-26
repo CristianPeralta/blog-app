8d0bcf7c870b78b1fc2d18027cb7b01abca6a421
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-26 07:21:29 -0500

    git add .

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -3,11 +3,31 @@ import React, { Component } from 'react';
 class Posts extends Component {
     state = {
         posts: [],
+        selectedPostId: null,
+        error: false,
+    }
+
+    postSelectedHandler(id) {
+        this.setState({
+            selectedPostId: id,
+        });
     }
     render() {
+        let posts = <p style={{textAlign: "center"}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+                );
+            });
+        }
         return (
             <section className="Posts">
-                {this.state.posts}
+                {posts}
             </section>
         );
     }
