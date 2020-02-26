8dbe8ba5f954bf548145a9ccf7398d8c60a9259c
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-26 09:02:48 -0500

    update postSelectedHandler to push to history

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from '../../../axios';
 
 import './Posts.css';
@@ -26,26 +25,22 @@ class Posts extends Component {
         })
         .catch(error => {
             console.log(error);
-            // this.setState({ error: true });
         });
     }
 
     postSelectedHandler(id) {
-        this.setState({
-            selectedPostId: id,
-        });
+        this.props.history.push('/' + id);
     }
     render() {
         let posts = <p style={{textAlign: "center"}}>Something went wrong!</p>
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
                 );
             });
         }
