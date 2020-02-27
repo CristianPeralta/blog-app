5c36529c206344d8cdfdb757cce477b5b0102d1b
CristianPeralta <cbperaltasegura@gmail.com>
2020-02-27 08:51:34 -0500

    import FullPost lazily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-
+// import NewPost from './NewPost/NewPost';
 import './Blog.css';
+
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 
 class Blog extends Component {
     state = {
@@ -26,7 +32,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     {/*<Redirect from="/" to="/posts" />*/}
                     <Route render={() => <h1>Not Found</h1>} />
